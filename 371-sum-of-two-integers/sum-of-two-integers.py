05c3023b1d94ed1d27f199c2a55f056e7c43ab56
Priya JA <priyaaiyer2004@gmail.com>
2024-07-09 14:44:09 +0530

    Time: 19 ms (15.62%) | Memory: 11.5 MB (58.16%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        l=[]
+        l.append(a)
+        l.append(b)
+        return sum(l)
