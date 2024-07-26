c8c0716fcac38b114434edebe08e535be84adfa8
Priya JA <priyaaiyer2004@gmail.com>
2024-07-26 20:13:19 +0530

    Time: 27 ms (40.87%) | Memory: 12 MB (70.12%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        l=[]
+        for i in matrix:
+            for j in i:
+                l.append(j)
+        if target in l:
+            return True
