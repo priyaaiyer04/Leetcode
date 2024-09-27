66043adaf3bfaa2fb0024ebcf76620648f79252e
Priya JA <priyaaiyer2004@gmail.com>
2024-09-27 22:08:43 +0530

    Time: 47 ms (44.27%) | Memory: 11.7 MB (73.33%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -9,27 +9,30 @@ class Solution(object):
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
-        
         """
-        s=""
         s1=""
-        while(l1):
-            s+=str(l1.val)
-            l1=l1.next
-        while(l2):
-            s1+=str(l2.val)
-            l2=l2.next
-        x=int(s[::-1])+int(s1[::-1])
-        x=str(x)
-        x=x[::-1]
-        t=ListNode(int(x[0]))
-        t1=t
-        t2=t
-        for i in x[1:len(x)]:
-            t=ListNode(int(i))
-            t2.next=t
-            t2=t
-        return t1
-
-           
-           
+        s2=""
+        t1=l1
+        t2=l2
+        while(t1):
+        
+            s1+=str(t1.val)
+            t1=t1.next
+        
+        while(t2):
+        
+            s2+=str(t2.val)
+            t2=t2.next
+        
+        s3=int(s1[::-1])+int(s2[::-1])
+        s3=str(s3)[::-1]
+        l3=ListNode(s3[0])
+        s3=s3[1:len(s3)]
+        t=l3
+        
+        while(s3):
+            l4=ListNode(s3[0])
+            s3=s3[1:len(s3)]
+            t.next=l4
+            t=l4
+        return l3
