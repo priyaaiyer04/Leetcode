83abaad989a27504c21b9d19565b260272db6364
Priya JA <priyaaiyer2004@gmail.com>
2024-10-07 21:49:02 +0530

    Time: 28 ms (61.11%) | Memory: 11.5 MB (97.78%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minLength(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        while True:
+            if "AB" in s:
+                s=s.replace("AB","")
+            elif "CD" in s:
+                s=s.replace("CD","")    
+            else:
+                break
+        return len(s)
