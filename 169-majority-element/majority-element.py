6eee5687baa6bf5d264d33582f75c1ee1a349d37
Priya JA <priyaaiyer2004@gmail.com>
2025-03-30 00:21:52 +0530

    Time: 23 ms (11.30%) | Memory: 13.7 MB (31.48%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,4 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return max(set(nums),key=nums.count)
+        d={}
+        l=[]
+        if nums.count(nums[0])==len(nums):
+            return nums[0]
+        for i in nums:
+            if i not in list(d.keys()):
+                d[i]=nums.count(i)
+        for i in d.keys():
+            l.append(d[i])
+        x=max(l)
+        i=l.index(x)
+        l1=list(d.keys())
+        return l1[i]
