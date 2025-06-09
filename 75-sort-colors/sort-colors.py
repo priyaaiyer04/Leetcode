f087b6f304a9b874f10b8784e7ad3d215754ded5
Priya JA <priyaaiyer2004@gmail.com>
2025-06-09 15:38:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (52.81%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,4 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        return nums.sort()
+        if len(nums)==1 or len(nums)==0:
+            return nums
+        x=min(nums)
+        y=max(nums)
+        count=[0]*(y-x+1)
+        for i in nums:
+            count[i-x]+=1
+        k=0
+        for i in range(len(count)):
+            for j in range(count[i]):
+                nums[k]=i+x
+                k+=1
+        return nums
