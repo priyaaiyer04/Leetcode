11cdaaee97fcbc0e9e5fcfe2dc2d3a5c51f3f7b3
Priya JA <priyaaiyer2004@gmail.com>
2025-07-26 12:17:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (97.14%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -5,13 +5,14 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         res=[]
-        def helper(curr,i):
+        def h(curr,i):
             if i==len(nums):
                 res.append(curr[:])
                 return
+           
             curr.append(nums[i])
-            helper(curr,i+1)
+            h(curr,i+1)
             curr.pop()
-            helper(curr,i+1)
-        helper([],0)
+            h(curr,i+1)
+        h([],0)
         return res
