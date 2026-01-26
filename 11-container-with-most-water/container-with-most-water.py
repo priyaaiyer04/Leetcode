6113cf4d20cb0ddc0fdde89284dfd609892c13ba
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-01-26 12:04:56 +0530

    Time: 123 ms (27.02%) | Memory: 20.8 MB (19.29%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -5,18 +5,13 @@ class Solution(object):
         :rtype: int
         """
         
-        left, right = 0, len(height) - 1
-        max_area = 0
-        
-        while left < right:
-            # Calculate the area with the current pair of heights
-            area = min(height[left], height[right]) * (right - left)
-            max_area = max(max_area, area)
-            
-            # Move the pointer corresponding to the smaller height
-            if height[left] < height[right]:
-                left += 1
+        l=0
+        r=len(height)-1
+        area=0
+        while l<r:
+            area=max(area,abs(r-l)*min(height[l],height[r]))
+            if height[l]<height[r]:
+                l+=1
             else:
-                right -= 1
-                
-        return max_area
+                r-=1
+        return area
