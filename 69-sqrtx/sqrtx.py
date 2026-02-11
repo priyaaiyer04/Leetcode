b0719bcd0ebc250e7ae9b0e7955d79efca1abdaa
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-02-11 22:42:00 +0530

    Time: 231 ms (16.71%) | Memory: 12.5 MB (18.46%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,26 @@
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        l=0
+        u=x//2+1
+        print(u)
+        while l<=u:
+            m=(l+u)//2
+            print(l,u,m)
+            if m*m==x:
+                
+                return m
+                break
+            elif m*m>x:
+                u=m-1
+            elif m*m<x:
+                if (m+1)*(m+1)>x:
+                    
+                    return m
+                    break
+                l=m+1
+        return m
