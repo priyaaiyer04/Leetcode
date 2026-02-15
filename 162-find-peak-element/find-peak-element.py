d27734c22f6de644f7ebb3227f30f80008b3a52a
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-02-15 12:22:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (86.48%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -4,10 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        for i in range(1,len(nums)-1):
-            if nums[i]>nums[i-1] and nums[i]>nums[i+1]:
-                return i
-        if len(nums)>1 and nums[0]>nums[1] or len(nums)==1:
-            return 0
-        if nums[len(nums)-1]>nums[len(nums)-2]:
-            return len(nums)-1
+        l=0
+        r=len(nums)-1
+        while l<r:
+            m=(l+r)//2
+        
+            if nums[m]<nums[m+1]:
+                l=m+1
+            else:
+                r=m
+        return r
+
+
+                
