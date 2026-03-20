f7961ae48482f8c034060cb7bcebe2085c7aa1d7
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-03-20 13:02:56 +0530

    Time: 887 ms (5.08%) | Memory: 34.6 MB (5.13%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,31 +1,25 @@
 import heapq
-
 class Solution(object):
     def maxSlidingWindow(self, nums, k):
-        if k == 1:
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        if k==1:
             return nums
-        
-        heap = []
-        ans = []
-        
-        # build first window
-        for i in range(k):
-            heapq.heappush(heap, (-nums[i], i))
-        
+        if k==len(nums):
+            return [max(nums)]
+        heap=[]
+        ans=[]
+        for i in range(0,k):
+            heapq.heappush(heap,(-nums[i],i))
         ans.append(-heap[0][0])
-        
-        l = 1
-        for r in range(k, len(nums)):
-            # push new element
-            heapq.heappush(heap, (-nums[r], r))
-            
-            # remove stale elements
-            while heap and heap[0][1] < l:
+        l=1
+        for r in range(k,len(nums)):
+            heapq.heappush(heap,(-nums[r],r))
+            while heap and heap[0][1]<l :
                 heapq.heappop(heap)
-            
-            # take max
             ans.append(-heap[0][0])
-            
-            l += 1
-        
-        return ans
+            l+=1
+        return ans 
