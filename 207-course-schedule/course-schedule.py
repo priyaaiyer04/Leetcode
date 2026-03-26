329e5c870dee5a2f289915ad1be0ac6e3126403b
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-03-26 22:21:49 +0530

    Time: 12 ms (25.28%) | Memory: 16.5 MB (5.98%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -5,27 +5,32 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-        d={}
-        for i in prerequisites:
-            if i[0] not in d.keys():
-                d[i[0]]=[]
-            d[i[0]].append(i[1])
-            if i[1] not in d.keys():
-                d[i[1]]=[]
-        visitedpast=set()
-        def detectcycle(d,src,visited):
-            if src in visitedpast:
-                return False
-            if src in visited:
-                return True
-            else:
-                visited.append(src)
-                for i in d[src]:
-                        if detectcycle(d,i,visited):
-                            return True
-            visitedpast.add(src)
+        def dfs(n,visted,path):
+            visited[n]=1
+            path[n]=1
+            for i in d[n]:
+                if visited[i]==0:
+                    if dfs(i,visited,path)==True:
+                        return True
+                elif path[i]==1:
+                    return True
+            path[n]=0
             return False
+        d={}
+        
+        for i in prerequisites:
+            if i[1] in d:
+                if i[0] not in d[i[1]]:
+                    d[i[1]].append(i[0])
+            elif i[1] not in d:
+                d[i[1]]=[i[0]]
+            if i[0] not in d:
+                d[i[0]]=[]
+        visited={i:0 for i in d}
+        path={i:0 for i in d}
+        
         for i in d.keys():
-            if detectcycle(d,i,[])==True:
-                return False
+            if visited[i]==0:
+                if dfs(i,visited,path)==True: #cycle is there
+                    return False 
         return True
