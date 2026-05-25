d341057eb7a9843310fafeaf8fed90fc0e52ed1e
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-05-25 20:22:20 +0530

    Time: 1228 ms (16.74%) | Memory: 18.6 MB (27.68%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,27 +4,41 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        ans=[]
+        
         nums.sort()
-        i=0
-        k=len(nums)-1
-        j=len(nums)-2
-        while i<j<k:
-            while i<j:
-                target=-nums[k]
-                if nums[i]+nums[j]==target:
-                    l=[nums[i],nums[j],nums[k]]
-                    l.sort()
-                    if l not in ans:
-                        ans.append(l)
-                    i+=1
-                    j-=1
-                elif nums[i]+nums[j]<target:  
-                    i+=1
-                elif nums[i]+nums[j]>target:  
-                    j-=1
-            i=0
-            k-=1
-            j=k-1
+        ans=[]
+        
+        for i in range(len(nums)):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+           
+            target=-1*nums[i]
+            left=i+1
+            right=len(nums)-1
+            while left<right:
+                if left<right and nums[left]+nums[right]<target:
+                    x=left+1
+                    while  x<len(nums) and nums[x]==nums[left]:
+                        x+=1
+                    left=x
+                elif left<right and nums[left]+nums[right]>target:
+                    x=right-1
+                    while x>=0 and nums[x]==nums[right]:
+                        x-=1
+                    right=x    
+                elif left<right and nums[left]+nums[right]==target:
+                    l1=[]
+                    if left!=right and right!=i and left!=i:
+                        l1.append(nums[left])
+                        l1.append(nums[right])
+                        l1.append(nums[i])
+                        ans.append(l1)
+                    x=right-1
+                    while x>=0 and nums[x]==nums[right]:
+                        x-=1
+                    right=x
+                    x=left+1
+                    while x<len(nums) and nums[x]==nums[left]:
+                        x+=1
+                    left=x
         return ans
-              
