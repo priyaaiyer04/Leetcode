0ebc3e80b934ad5c59d25f9f5423dc8495df25fd
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-05-26 18:35:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (90.56%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,23 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        i=0
-        c=0
-        while i<len(nums):
-            if nums[i]==0:
-                c+=1
-            i+=1
-        for i in range(c):
-            nums.remove(0)
-        for i in range(c):
-            nums.insert(0,0)
-        i=0
-        c=0
-        while i<len(nums): 
-            if nums[i]==2:
-                c+=1
-            i+=1
-        for i in range(c):
+        x=nums.count(2)
+        y=nums.count(0)
+        for i in range(x):
             nums.remove(2)
-        for i in range(c):
+        
+        for i in range(x):
+        
             nums.append(2)
+        for i in range(y):
+            
+            nums.remove(0)
+        for i in range(y):
+            
+            nums.insert(0,0) 
