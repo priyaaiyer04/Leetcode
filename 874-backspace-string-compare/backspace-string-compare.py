79ef24a7bb472ce96a3ca0cdc59a46112534ad2b
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-05-27 21:44:32 +0530

    Time: 9 ms (5.14%) | Memory: 12.3 MB (89.07%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -5,22 +5,19 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        s1=[]
+        l=[]
         for i in s:
-            print(s1)
-            if i=="#":
-                if len(s1)>0:
-                    s1.pop()
-            else:
-                s1.append(i)
-        t1=[]
+            if i=="#" and len(l)>0:
+                l.pop()
+            elif i!="#":
+                l.append(i)
+        l1=[]
         for i in t:
-            print(t1)
-            if i=="#":
-                if len(t1)>0:
-                    t1.pop()
-            else:
-                t1.append(i)
-        if s1==t1:
+            if i=="#" and len(l1)>0:
+                l1.pop()
+            elif i!="#":
+                l1.append(i)
+        print(l,l1)
+        if l==l1:
             return True
         return False
