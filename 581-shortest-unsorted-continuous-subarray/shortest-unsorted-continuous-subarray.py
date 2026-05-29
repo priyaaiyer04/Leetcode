2ab763efed33bd6c2b09835b55d21f8fb060f962
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-05-29 13:36:23 +0530

    Time: 7 ms (89.87%) | Memory: 13.5 MB (10.57%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
@@ -4,12 +4,33 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        l=[i for i in nums]
-        l.sort()
-        ans=[]
-        for i in range(len(nums)):
-            if nums[i]!=l[i]:
-                ans.append(i)
-        if len(ans)<=0:
+        if len(nums)<=1:
             return 0
-        return (max(ans)-min(ans)+1)
+        l=0
+        r=len(nums)-1
+        while  l<len(nums)-1 and nums[l]<=nums[l+1]:
+                l+=1
+        while r>0 and nums[r]>=nums[r-1] :
+                r-=1
+        if r==0 or l==len(nums)-1:
+            return 0
+        x=min(nums[l:r+1])
+        y=max(nums[l:r+1])
+        l1=l
+        r1=r
+        while l1>=0 and r1<len(nums):
+            if nums[l1]>x:
+                l=l1
+            if nums[r1]<y:
+                r=r1
+            l1-=1
+            r1+=1
+        while l1>=0 :
+            if nums[l1]>x:
+                l=l1
+            l1-=1
+        while r1<len(nums):
+            if nums[r1]<y:
+                r=r1
+            r1+=1
+        return r-l+1
