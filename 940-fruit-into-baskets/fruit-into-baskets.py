67d34144d17c800b286554f26a1ebda773e57498
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-06-08 11:52:21 +0530

    Time: 253 ms (70.90%) | Memory: 17.8 MB (6.95%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -4,28 +4,39 @@ class Solution(object):
         :type fruits: List[int]
         :rtype: int
         """
-        l=0
-        r=0
-        ans=0
-        l1=[]
-        while l<=r<len(fruits):
-            
+        ans = 0
+        l = 0
+        r = 0
+        l1 = []
+        if len(fruits)==fruits.count(fruits[0]):
+            return len(fruits)
+        while l <= r < len(fruits):
             if fruits[r] in l1:
-                r+=1
-            elif fruits[r] not in l1 and len(l1)<2:
                 l1.append(fruits[r])
-                r+=1
-            elif fruits[r] not in l1 and len(l1)==2:
-                print(l1,fruits[r],r)
-                ans=max(ans,r-l)
-                if r-l>=len(fruits)-(l+1):
-                    break
-                x=fruits[l]
-                while fruits[l]==x:
-                    l+=1
-                l1.remove(x)
-                r=l
-                print(l1,fruits[r],r)
-        if len(l1)<=2:
-            ans=max(ans,r-l)   
+                r += 1
+
+            elif fruits[r] not in l1 and (
+                len(l1) == 0 or len(l1) == l1.count(l1[0])
+            ):
+                l1.append(fruits[r])
+                r += 1
+
+            elif fruits[r] not in l1 and len(l1) != l1.count(l1[0]):
+                
+                ans = max(ans, r - l)
+
+                l1 = l1[::-1]
+                x = l1[0]
+                idx=0
+                while idx<len(l1) and  l1[idx]==x:
+                    idx+=1
+                idx = len(l1) - idx
+                
+                l1 = l1[::-1]
+                l1 = l1[idx:]
+                l += idx
+
+            
+
+        ans = max(ans, r - l)
         return ans
