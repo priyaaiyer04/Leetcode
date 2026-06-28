2725f637f8d83a5af26c376115a9967330ec2e90
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-06-28 15:02:00 +0530

    Time: 19 ms (5.50%) | Memory: 30 MB (96.74%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -4,86 +4,56 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
-    
     def reorderList(self, head):
         """
         :type head: Optional[ListNode]
         :rtype: None Do not return anything, modify head in-place instead.
         """
-        if head.next==None:
-            return head
-        if head==None:
-            return head
+        
+
+        
+
         s=head
         f=head
-        while f:
+        p=s
+        while f and f.next!=None:
+            p=s
             s=s.next
-            if f.next and f.next.next:
-                f=f.next.next
+            if f and f.next!=None:
+                f=f.next
+            if f and f.next!=None:
+                f=f.next
+        if p:
+            p.next=None
+        if f==head:
+            return
+        head1=f
+        t=s
+        while t!=f:
+            if s==t:
+                t=t.next
+                s.next=f.next
+                f.next=s
+            else:
+                x=t.next
+                t.next=f.next
+                f.next=t
+                t=x
+
+        s=head
+        f=head1
+        while s and f:
+            x=f.next
+            f.next=s.next
+            s.next=f
+            f=x
+            if s.next!=None:
+                s=s.next
+            if s.next!=None:
+                s=s.next
             else:
                 break
-        l2=ListNode(s.val)
-        t=s
-        t2=l2
-        while t.next:
-            t=t.next
-            t1=ListNode(t.val)
-            t2.next=t1
-            t2=t1
-        t=head
-        t1=t
-        while t!=s:
-            t1=t
-            t=t.next
-        t1.next=None
-        t=head
-        def rev(head):
-            tail=head
-            if head is None:
-                return head
-            if head.next is None:
-                return head
-            while tail.next:
-                tail=tail.next
-        
-            if head.next is tail:
-                head.next=None
-                tail.next=head
-                head=tail
-                return head
-            else:
-                while head!=tail:
-                    if tail.next:
-                        t=head
-                        head=head.next
-                        t.next=tail.next
-                        tail.next=t
-                    else:
-                        t=head
-                        tail.next=head
-                        head=head.next
-                        t.next=None
-                return head
-    
-        l2=rev(l2)
-        t=head
-        t1=l2
-        while t1 and t:
-            print(t.val,t1.val)
-            if t.next:
-                if t1.next:
-                    x=t1.next
-                else:
-                    x=None
-                t1.next=t.next
-                
-                t.next=t1
-                t1=x
-                if t.next.next:
-                    t=t.next.next
             
-            else:
-                t.next=t1
-                t=t.next
-                t1=t1.next
-        return head
+        if f:
+            s.next=f
+            f.next=None
