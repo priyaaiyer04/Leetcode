ee95ea46f24d0c422cc5faea0fcd06415be6f8d4
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-07-04 17:28:26 +0530

    Time: 22 ms (10.28%) | Memory: 14.4 MB (7.83%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -9,28 +9,23 @@ class Solution(object):
                 break
         l1=nums[:i]
         l2=nums[i:]
-        l1=[abs(i) for i in l1]
+        l1=[i**2 for i in l1]
+        l2=[i**2 for i in l2]
         l1=l1[::-1]
-        l=0
-        r=0
+        t=0
+        t1=0
         ans=[]
-        while l<len(l1) and r<len(l2):
-            if l2[r]<l1[l]:
-                ans.append(l2[r])
-                r+=1
-            elif l2[r]>l1[l]:
-                ans.append(l1[l])
-                l+=1
+        while t<len(l1) and t1<len(l2):
+            if l1[t]<l2[t1]:
+                ans.append(l1[t])
+                t+=1
             else:
-                ans.append(l1[l])
-                ans.append(l2[r])
-                l+=1
-                r+=1
-        while l<len(l1):
-            ans.append(l1[l])
-            l+=1
-        while r<len(l2):
-            ans.append(l2[r])
-            r+=1
-        ans=[i**2 for i in ans]
+                ans.append(l2[t1])
+                t1+=1
+        while t<len(l1):
+            ans.append(l1[t])
+            t+=1
+        while t1<len(l2):
+            ans.append(l2[t1])
+            t1+=1
         return ans
