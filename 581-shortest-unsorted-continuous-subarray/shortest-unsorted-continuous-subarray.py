ba35b75c8a61f3e94e6c94df03f18c28a0a90d72
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-07-09 23:23:30 +0530

    Time: 12 ms (80.00%) | Memory: 13.4 MB (30.37%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
@@ -4,33 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums)<=1:
-            return 0
         l=0
-        r=len(nums)-1
-        while  l<len(nums)-1 and nums[l]<=nums[l+1]:
-                l+=1
-        while r>0 and nums[r]>=nums[r-1] :
-                r-=1
-        if r==0 or l==len(nums)-1:
-            return 0
-        x=min(nums[l:r+1])
-        y=max(nums[l:r+1])
-        l1=l
-        r1=r
-        while l1>=0 and r1<len(nums):
-            if nums[l1]>x:
-                l=l1
-            if nums[r1]<y:
-                r=r1
-            l1-=1
-            r1+=1
-        while l1>=0 :
-            if nums[l1]>x:
-                l=l1
-            l1-=1
-        while r1<len(nums):
-            if nums[r1]<y:
-                r=r1
-            r1+=1
-        return r-l+1
+        r=0
+        for i in range(len(nums)-1):
+            if nums[i]>nums[i+1]:
+                l=i
+                break
+        for i in range(len(nums)-1,0,-1):
+            if nums[i]<nums[i-1]:
+                r=i
+                break
+        min1=min(nums[l:r+1])
+        max1=max(nums[l:r+1])
+        c=l
+        while l>=0 :
+            if nums[l]>min1:
+                c=l
+            l-=1
+        c1=r
+        while r<len(nums) :
+            if nums[r]<max1:
+                c1=r
+            r+=1
+        if (c==0 and c1==0):
+            return 0        
+        return abs(c-c1)+1
