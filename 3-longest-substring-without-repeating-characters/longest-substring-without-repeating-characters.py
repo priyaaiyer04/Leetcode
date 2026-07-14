740f0059be62006039e92576327889bceaf5f286
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-07-14 11:00:52 +0530

    Time: 109 ms (14.93%) | Memory: 12.5 MB (80.92%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,28 +4,29 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        ans=0
+        
         l=0
         r=0
-        s1=""
+        ans=0
+        l1=[]
         while l<=r<len(s):
-            if s[r] in s1:
+            if s[r] in l1:
                 
-                ans=max(ans,len(s1))
-                x=l
-                list1=list(s[l:r])
-                list1=list1[::-1]
-                idx=list1.index(s[r])
-                idx=len(list1)-idx+l-1
+                ans=max(ans,r-l)
+                oldl=l
                 
-                l=idx+1
-                print(idx)
-                s1=s[l:r+1]
+                x=s[l:r+1].index(s[r])
+                
+                l+=x
+                if l==oldl:
+                    l+=1
+                for i in s[oldl:l]:
+                    l1.remove(i)
+                if r<l:
+                    r=l
+            elif s[r] not in l1:
+                l1.append(s[r])
                 r+=1
-                
-            elif s[r] not in s1:
-                s1+=s[r]
-                r+=1
-            print(l,r,s1)
-        ans=max(ans,len(s1))
+           
+        ans=max(ans,r-l)
         return ans
