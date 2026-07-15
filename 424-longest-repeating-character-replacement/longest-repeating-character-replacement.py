d045dfc2dd80731f30f82452c26e52a3b505f62b
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-07-15 13:53:58 +0530

    Time: 318 ms (7.56%) | Memory: 12.7 MB (95.22%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -6,24 +6,36 @@ class Solution(object):
         :rtype: int
         """
         
-        d={}
-        for i in s:
-            d[i]=0
         l=0
         r=0
-        max1=-1
         ans=0
+        d={}
+        max_c=s[0]
+        for i in s:
+            d[i]=0
         while l<=r<len(s):
-            d[s[r]]+=1
-            max1=max(d[s[r]],max1)
-            if r-l-max1>=k:
-                ans=max(ans,r-l)
-               
-                d[s[l]]-=1
-                l=l+1
-                r+=1
+
+            while max_c in d and r-l-d[max_c]<=k:
+                if r<len(s):
+                    d[s[r]]+=1
+                    if ( max_c in d.keys() and d[s[r]]>d[max_c]) or max_c=="":
+                        max_c=s[r]
+                    r+=1
+                    
+                elif r>=len(s):
+                    break
             else:
-                r+=1
-            
-        ans=max(ans,r-l)
+                ans=max(ans,r-l-1)
+                d[s[l]]-=1
+                for i in d:
+                    if (max_c in d and d[i]>d[max_c]) or max_c=="":
+                        max_c=i
+                if d[s[l]]<0:
+                    d[s[l]]=0
+                l+=1
+                if r<l:
+                    r=l
+        if len(s)-l-d[max_c]<=k:
+          
+            ans=max(ans,len(s)-l)      
         return ans
