5ab15218f1f993b58f01a5396d64586d072c7c7d
Priya Janaky Aiyer <priyaaiyer2004@gmail.com>
2026-07-16 17:54:53 +0530

    Time: 361 ms (5.02%) | Memory: 17.2 MB (5.98%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -4,39 +4,38 @@ class Solution(object):
         :type fruits: List[int]
         :rtype: int
         """
-        ans = 0
-        l = 0
-        r = 0
-        l1 = []
-        if len(fruits)==fruits.count(fruits[0]):
-            return len(fruits)
-        while l <= r < len(fruits):
-            if fruits[r] in l1:
-                l1.append(fruits[r])
-                r += 1
+        
+        
 
-            elif fruits[r] not in l1 and (
-                len(l1) == 0 or len(l1) == l1.count(l1[0])
-            ):
-                l1.append(fruits[r])
-                r += 1
-
-            elif fruits[r] not in l1 and len(l1) != l1.count(l1[0]):
-                
-                ans = max(ans, r - l)
-
-                l1 = l1[::-1]
-                x = l1[0]
-                idx=0
-                while idx<len(l1) and  l1[idx]==x:
-                    idx+=1
-                idx = len(l1) - idx
-                
-                l1 = l1[::-1]
-                l1 = l1[idx:]
-                l += idx
-
+        l=0
+        r=0
+        ans=0
+        d={}
+        while l<=r<len(fruits):
             
-
-        ans = max(ans, r - l)
+            
+            if fruits[r] in d.keys():
+                d[fruits[r]]+=1
+                r+=1
+            elif fruits[r] not in d.keys() and len(d)<2:
+            
+                d[fruits[r]]=1
+                r+=1
+            elif fruits[r] not in d.keys() and len(d)>=2:
+                ans=max(ans,r-l)
+                x=l
+                while len(d)>=2 and x<len(fruits):
+                    if len(d)<2:
+                        break
+                    d[fruits[x]]-=1
+                    if d[fruits[x]]==0:
+                        del d[fruits[x]]
+                    x+=1
+                l=x
+                if r<l:
+                    l=r
+            
+            
+        if len(d)<=2:
+            ans=max(ans,len(fruits)-l)
         return ans
