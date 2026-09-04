6cb9a619b5de752631645cbc836236144d2560a9
Priya J A <priyaaiyer2004@gmail.com>
2026-09-04 13:55:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (20.01%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -5,12 +5,12 @@ class Solution(object):
         :rtype: int
         """
         d={}
-        def c(n):
+        def stairs(n):
             if n==1 or n==2:
                 return n
             if n in d:
                 return d[n]
             else:
-                d[n]= c(n-1)+c(n-2)
+                d[n]=stairs(n-1)+stairs(n-2)
                 return d[n]
-        return c(n)
+        return stairs(n)
