c83b762d7fc773d998ed8b2ccc473eb144c7cfd7
Priya J A <priyaaiyer2004@gmail.com>
2026-09-05 11:52:07 +0530

    Time: 271 ms (14.33%) | Memory: 27.8 MB (48.69%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -4,21 +4,24 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        sum1=0
-        def count_island(r,c):
-            if r>=len(grid) or c>=len(grid[0]) or r<0 or c<0:
+        d={}
+        def dp(i,j):
+            if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]):
                 return 0
-            if grid[r][c]=="1":
-                    grid[r][c] = "0"
-                    count_island(r-1,c)
-                    count_island(r+1,c)
-                    count_island(r,c+1)
-                    count_island(r,c-1)
-                    return 1
+            if (i,j) in d:
+                return d[(i,j)]
+            if grid[i][j]=="1":
+                grid[i][j]=0
+                dp(i+1,j)
+                dp(i-1,j)
+                dp(i,j+1)
+                dp(i,j-1)
+                return 1
             else:
-                    return 0    
-            
+                return 0
+            return d[(i,j)]
+        ans=0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                sum1+=count_island(i,j)
-        return sum1
+                ans+=dp(i,j)
+        return ans
